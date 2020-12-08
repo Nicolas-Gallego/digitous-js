ab28ee68b856e18485f42aac647aff91a68e410c
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-08 15:18:58 +0100

    Bonus 2 terminé

--- jour2.js
+++ jour2.js
@@ -66,7 +66,49 @@ var max = 6
 var random = Math.floor(Math.random() * (max - min + 1) + min)
 console.log(random)
 if (random === 6) {
-    console.log("Yes I win")
+    console.log("Yes I win !")
 } else {
     console.log("So close...")
+}
+
+// Bonus II
+var month = "July"
+
+switch (month) {
+    case "January":
+        console.log("Winter");
+        break;
+    case "February":
+        console.log("Winter");
+        break;
+    case "March":
+        console.log("Winter");
+        break;
+    case "April":
+        console.log("Spring");
+        break;
+    case "May":
+        console.log("Spring");
+        break;
+    case "June":
+        console.log("Spring");
+        break;
+    case "July":
+        console.log("Summer");
+        break;
+    case "August":
+        console.log("Summer");
+        break;
+    case "September":
+        console.log("Summer");
+        break;
+    case "October":
+        console.log("Fall");
+        break;
+    case "November":
+        console.log("Fall");
+        break;
+    case "December":
+        console.log("Fall");
+        break;
 }
