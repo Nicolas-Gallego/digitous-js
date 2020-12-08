ef86b63b4c38ddb430be22d08b393e2cb5a26a09
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-08 14:14:50 +0100

    ça avance bien

---
+++ jour2.js
@@ -0,0 +1,29 @@
+// Number
+var integer = 102;
+var float = 13.9;
+console.log(integer, float);
+
+// Convert
+var basic = 34;
+var stringified = basic.toString();
+console.log(stringified);
+console.log(stringified.length);
+
+// Round
+var num = 1.5;
+var rounded = Math.round(1.5);
+console.log(rounded);
+
+// Arithmétique
+var test = 12;
+var bis = 5;
+console.log(test + bis);
+console.log(test - bis);
+console.log(test * bis);
+console.log(test / bis);
+console.log(test ** bis);
+console.log(test % bis);
+
+// Comparaison
+var test = 143
+var bis = 219
