69e873c910caed69da7b657a02084ce286642427
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-09 15:03:14 +0100

    Exercice 3 terminé

---
+++ jour3.js
@@ -0,0 +1,18 @@
+// 01 - Array
+var fruits = ["mango", "lemon", "blueberry"];
+console.log(fruits);
+
+// 02 - Access
+var ingredients = ["eggs", "milk", "butter"];
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
+
+// 03 - Add and Remove
+var objects = ["pen", "book", "lamp"];
+objects.unshift("chair");
+objects.pop();
+objects.push("laptop");
+objects.shift()
+console.log(objects);
+
+// 04 - Order
