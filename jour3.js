28117e539fbe2ae20878f3c756a1a14d272c9bd8
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-09 17:20:59 +0100

    Bonus peut-être réussi ?

--- jour3.js
+++ jour3.js
@@ -22,16 +22,37 @@ numbers.sort((a, b) => a - b);
 console.log(numbers);
 
 // 05 - Boucle
-var total = 0
-var limit = 10
-for (var total; total <= limit; total++) {
-    console.log(total)
+var total = 0;
+var limit = 10;
+for (var i = 1; i <= limit; i++) {
+    total += i;
 }
+console.log(total)
 
 // 06 - Reverse
 var sentence = "Hello Konexio !"
 var reverseStr =""
 for(var i = sentence.length -1; i >= 0; i--){
-reverseStr = reverseStr + sentence[i]
+reverseStr += sentence[i]
 }
 console.log(reverseStr)
+
+// Bonus
+for (i=1; i <= 100; i++){
+    if( i % 3 == 0 ){
+        console.log("fizz")
+    }
+    if( i % 5 == 0 ){
+        console.log("buzz")
+    }
+    if( ( i % 3 != 0 ) && ( i % 5 != 0 ) ){
+        console.log("fizzbuzz")
+    }
+    if( i % 7 != 0 ){
+    console.log("")
+    }
+    if( i % 2, 4, 6, 8, 9 != 0){
+        console.log(i)
+    }
+}
+
