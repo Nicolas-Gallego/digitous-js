17feb91f84a9edfe82d27080c0540b60f0f5cbc7
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-14 12:41:10 +0100

    exercice 2 terminé

--- forLoops.js
+++ forLoops.js
@@ -38,4 +38,19 @@ while (i < 201) {
     }
 }
 
-// 02
+// 02 Try again
+var dice = null
+var count = 0
+while()
+
+
+var box1 = 12;
+var box2 = 5;
+for (var i = 12; i < 14; i++) {
+    box1 = box2;
+    console.log(box1 + box2);
+    box2 = box1 + i;
+}
+console.log(box1);
+console.log(box2);
+console.log(i);
---
+++ jour4.js
@@ -0,0 +1,23 @@
+// 01 - Object
+var cat = {
+    name: "Garfiel",
+    age: 3,
+    isCute: true,
+}
+if (cat.isCute) {
+    console.log("So cute !")
+}
+// 02 - Combine
+var cat2 = {
+    name: "John",
+    age: 5,
+    isCute: false,
+}
+
+var cats = [cat, cat2]
+console.log(cat.age)
+console.log(cat2.isCute)
+
+// 03 - Even
+function checkIfEven(){
+}
