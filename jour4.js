9397c0cc10e84daaac39d14193b72862ea0eb4ce
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-14 14:32:14 +0100

    start ex 6

--- jour4.js
+++ jour4.js
@@ -7,6 +7,8 @@ var cat = {
 if (cat.isCute) {
     console.log("So cute !")
 }
+
+
 // 02 - Combine
 var cat2 = {
     name: "John",
@@ -18,6 +20,50 @@ var cats = [cat, cat2]
 console.log(cat.age)
 console.log(cat2.isCute)
 
+
 // 03 - Even
-function checkIfEven(){
+function checkIfEven(num) {
+    if (num % 2 === 0) {
+        console.log("Even")
+    }
+    else {
+        console.log("odd")
+    }
 }
+
+checkIfEven(1000)
+checkIfEven(34)
+checkIfEven(6597)
+checkIfEven(0991)
+
+
+// 04 - Compare
+function compare(num1, num2) {
+    if (num1 > num2) {
+        console.log(`${num1} is bigger`)
+    }
+    else if (num2 > num1) {
+        console.log(`${num2} is bigger`)
+    }
+    else {
+        console.log(`Both are the same`)
+    }
+}
+compare(2, 1)
+compare(2, 10)
+compare(2, 2)
+
+
+// 05 - Add Up
+var result = 0
+function addUp(num) {
+    for (var i = 1; i <= num; i++)
+    result += i
+    console.log(result)
+ }
+addUp(12)
+
+
+// 06 - Time
+
+
