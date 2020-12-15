f7b3708c80270f93cb0a7aacaf51aa718921934e
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-15 16:48:19 +0100

    exo 2 modifié

--- jour5-exo2.js
+++ jour5-exo2.js
@@ -1,14 +1,9 @@
-function multiply(num){
-    console.log(num * 1)
-    console.log(num * 2)
-    console.log(num * 3)
-    console.log(num * 4)
-    console.log(num * 5)
-    console.log(num * 6)
-    console.log(num * 7)
-    console.log(num * 8)
-    console.log(num * 9)
-    console.log(num * 10)
+function multiply(num) {
+    var resultx = 0
+    for (var i = 1; i <= 10; i++) {
+        resultx = num * i;
+        console.log(`${num} x ${i} = ${resultx}`)
+    }
 }
 console.log(process.argv)
 multiply(process.argv[2])
