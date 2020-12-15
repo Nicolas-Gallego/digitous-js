785953ee0ec7f15e1b62d982ed900c372bc89dfc
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-15 17:22:27 +0100

    exo 4 terminé ?

--- jour5-exo4.js
+++ jour5-exo4.js
@@ -3,4 +3,27 @@ prompt.start();
 var min = 1;
 var max = 100;
 var mysteryNum = Math.floor(Math.random() * (max - min + 1) + min);
-function play 
+console.log(`le numero mystère est ${mysteryNum}`)
+function play() {
+    var schema = {
+        properties: {
+            number: {
+                pattern: /^[0-9]+$/,
+                message: 'Le nombre doit etre un nombre',
+                required: true
+            },
+        }
+    };
+    prompt.get(schema, function (err, result) {
+        console.log(result)
+        if (mysteryNum > result.number){
+            console.log("C'est plus !")
+        }
+        else if (mysteryNum < result.number){
+            console.log("C'est moins !")
+        }
+        else {
+            console.log("BRAVO !")
+        }
+    })
+}
