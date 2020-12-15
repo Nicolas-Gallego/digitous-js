b9a3336bee9f2cd85021ea795785dc6180da3f83
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-15 16:27:01 +0100

    début exo 4, je suis en sueur

--- jour5-exo4.js
+++ jour5-exo4.js
@@ -1,5 +1,6 @@
 var prompt = require("prompt");
-var min = 1
-var max = 100
-var mysteryNum = Math.floor(Math.random() * (max - min + 1) + min)
+prompt.start();
+var min = 1;
+var max = 100;
+var mysteryNum = Math.floor(Math.random() * (max - min + 1) + min);
 function play 
