1d8c721aa611625dd7e6021890616b80f6922789
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-15 16:00:28 +0100

    amelioration utils

--- table-utils.js
+++ table-utils.js
@@ -1,12 +1,13 @@
 function multiply(num) {
-    var result = 0
+    var resultx = 0
     for (var i = 1; i <= 10; i++) {
-        result = num * i;
-        console.log(result)
+        resultx = num * i;
+        console.log(`${num} x ${i} = ${resultx}`)
     }
 }
-
+var result = 0
 function adition(num1, num2) {
-    console.log(num1 + num2)
+    var result = num1 + num2
+    console.log(`${num1} + ${num2} = ${result}`)
 }
 module.exports = { multiply, adition }
