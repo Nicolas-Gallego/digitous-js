01c199f8aa7913e43b85b7195501af886717da40
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-17 12:07:54 +0100

    début exo 2

---
+++ jour7.js
@@ -0,0 +1,17 @@
+// 01 - Alphabet
+console.log("01 - Alphabet")
+function sortLetters(string) {
+    var arr = string.split('');
+    var sorted = arr.sort();
+    console.log(sorted.join(''));   
+}
+sortLetters("konexio")
+
+
+
+// 02 - XOXO
+console.log("02 - XOXO")
+
+
+
+
