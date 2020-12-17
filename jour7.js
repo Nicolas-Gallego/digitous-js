e2ede465936842701547687b9d23673621297ae8
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2020-12-17 14:19:29 +0100

    exo 3 terminé

--- jour7.js
+++ jour7.js
@@ -12,6 +12,7 @@ sortLetters("Konexio")
 // 02 - XOXO
 console.log("02 - XOXO")
 function countEach(string) {
+    console.log(`Autant de O que de X dans ${string} ?`)
     var x = 0;
     var o = 0;
     var stringlength = string.length
@@ -19,17 +20,37 @@ function countEach(string) {
         if (string.charAt(i) === "x") {
             x += 1
         }
-        else if (string.charAt(i) === "o"){
+        else if (string.charAt(i) === "o") {
             o += 1
         }
-        else if (x === o){
+        else if (x === o) {
             console.log("true")
         }
-        else if (x !== o){
+        else if (x !== o) {
             console.log("false")
         }
     }
 }
 countEach("xox")
+countEach("xxxoooox")
 
+// 03 - Palindrome
+console.log("03 - Palindrome")
+
+function checkPal(string) {
+    console.log(`Est-ce que \"${string}\” est un palindrome ?`)
+    var lowRegStr = string.toLowerCase();
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    if (reverseStr === lowRegStr){
+        console.log("Palindrome !")
+    }
+    else {
+        console.log("Nope")
+    }
+  }
+  checkPal("raceCar");
+  checkPal("laptop");
+  checkPal("unDroleDeLordNu");
+
+  
 
