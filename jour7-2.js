2177725946c5f26838532a37e2c3f6c77a24dedf
Nicolas Gallego <gallagan@MacBook-Pro-de-Nicolas.local>
2021-01-04 15:39:36 +0100

    exo terminés

--- jour7-2.js
+++ jour7-2.js
@@ -38,8 +38,8 @@ console.log("----------");
 // 04 - Filter Numbers
 console.log("04 - Filter Numbers");
 var array = [1, "toto", 34, "javascript", 8, -5]
-var numbers = array.filter(function(element){
-    if (typeof element === "number"){
+var numbers = array.filter(function (element) {
+    if (typeof element === "number") {
         return element
     }
 })
@@ -49,9 +49,48 @@ console.log("-----------");
 // 05 - Filter Even
 console.log("05 - Filter Even");
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-var even = numbers.filter(function(element){
-    if (element % 2 === 0){
+var even = numbers.filter(function (element) {
+    if (element % 2 === 0) {
         return element
     }
 })
 console.log(even);
+console.log("-----------");
+
+// 06 - Cakes
+console.log("06 - Cakes");
+var cakes = [
+    {
+        name: "cake",
+        flavor: "vanilla",
+        status: "available"
+    },
+    {
+        name: "brownie",
+        flavor: "chocolate",
+        status: "available"
+    },
+    {
+        name: "pie",
+        flavor: "strawberry",
+        status: "available"
+    },
+    {
+        name: "muffin",
+        flavor: "pistachio",
+        status: "available"
+    },
+    {
+        name: "donut",
+        flavor: "chocolate",
+        status: "available"
+    },
+]
+var dispo = cakes.filter(function (element) {
+    if (element.flavor === "chocolate") {
+        element.status = "sold out"
+    return element
+    }
+    
+})
+console.log(dispo)
